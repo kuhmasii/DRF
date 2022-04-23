30a910fbe9b9c5dca55ca59f854a345b762ff652
kuhmasii <olaisaiah54@gmail.com>
2022-04-23 05:55:54 +0100

    added serializer file

---
+++ backend/backend_api/school/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import School
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    discount = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = School
+        fields = [
+            "name",
+            "location",
+            "founded",
+            '_price',
+            "discount",
+        ]
+
+    def get_discount(self, obj):
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, School):
+            return None
+        return obj.get_price_discount
